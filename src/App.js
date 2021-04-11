0b9293d2e0aeda7113d4fd6ce7de8f75cb38f455
S4UR000N <backtrack.grey@gmail.com>
2021-04-11 18:30:20 +0200

    basic project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react"
+import { useLocalStore, useObserver } from "mobx-react"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App container">
+        <p>bugs</p>
+    </main>
   );
 }
 
 export default App;
+
+/*
+1. Decode VIN
+2. Decode VIN (flat format)
+3. Decode VIN Extended
+4. Decode VIN Extended (flat format)
+5. Decode WMI
+6. Get WMIs for Manufacturer
+7. Get All Makes
+
+*/
